ce188922224b723deb3d1eb49db2c6fbc4623136
Evgeny Kuizhev <46845084+ekuizhev@users.noreply.github.com>
2020-03-27 12:02:42 +0300

    Update docker-linux-mint-19.sh

--- docker-linux-mint-19.sh
+++ docker-linux-mint-19.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y docker-ce
 
 # https://docs.docker.com/compose/install/
 # https://github.com/docker/compose/releases
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.26.0-rc3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # https://docs.docker.com/install/linux/linux-postinstall/
